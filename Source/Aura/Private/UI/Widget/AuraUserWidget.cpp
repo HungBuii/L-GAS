b00e8f1eda71b632aab03ad795ad270e3d6c3198
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-11-13 11:25:15 +0700

    create "UI UserWidget, Controller"

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Hung ne
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
