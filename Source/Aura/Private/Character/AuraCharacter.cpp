529ef4ae79a42e3d5b9d0b0fa0036911119a0210
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-12-08 15:37:58 +0700

    update: GE delegates (ASC) called on 'server' with all Actors

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -4,6 +4,7 @@
 #include "Character/AuraCharacter.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "Player/AuraPlayerController.h"
 #include "Player/AuraPlayerState.h"
@@ -41,7 +42,16 @@ void AAuraCharacter::InitAbilityActorInfo()
 {
 	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
 	check(AuraPlayerState);
+	/*
+	 *	InitAbilityActorInfo(): the structure that holds information about who we are acting on and who controls us.
+	 *	
+	 *	OwnerActor: is the actor that logically owns this component. 
+	 *	
+	 *	AvatarActor: is what physical actor in the world we are acting on. Usually a Pawn but it 
+	 *	could be a Tower, Building, Turret, etc, may be the same as Owner
+	 */
 	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
+	Cast<UAuraAbilitySystemComponent>(AuraPlayerState->GetAbilitySystemComponent())->AbilityActorInfoSet();
 	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
 	AttributeSet = AuraPlayerState->GetAttributeSet();
 
--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -11,9 +11,17 @@
  * Ability System Component (ASC): Attaches to Actor to manage and execute Gameplay Abilities, 
  *									and handles replication for multiplayer.
  */
+
 UCLASS()
 class AURA_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
+	
+public:
+	void AbilityActorInfoSet();
+	
+protected:
+	void EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, 
+		FActiveGameplayEffectHandle ActiveEffectHandle);
 	
 };
