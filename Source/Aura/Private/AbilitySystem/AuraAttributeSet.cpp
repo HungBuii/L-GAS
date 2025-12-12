70a56cdbd4473fa3173a42a35035131f645ee751
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2025-12-12 15:24:43 +0700

    fix: clamp attributes when it exceeds the max value

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -107,6 +107,21 @@ void UAuraAttributeSet::PostGameplayEffectExecute(const struct FGameplayEffectMo
 
 	FEffectProperties Props;
 	SetEffectProperties(Data, Props);
+
+	if (Data.EvaluatedData.Attribute == GetHealthAttribute())
+	{
+		// GEngine->AddOnScreenDebugMessage(1, 3.f, FColor::Red, 
+		// 	FString::Printf(TEXT("Health: %f"), GetHealth()));
+		
+		SetHealth(FMath::Clamp(GetHealth(), 0.f, GetMaxHealth()));
+	}
+	if (Data.EvaluatedData.Attribute == GetManaAttribute())
+	{
+		// GEngine->AddOnScreenDebugMessage(1, 3.f, FColor::Red, 
+		// 	FString::Printf(TEXT("Health: %f"), GetHealth()));
+    		
+		SetMana(FMath::Clamp(GetMana(), 0.f, GetMaxMana()));
+	}
 }
 
 void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
