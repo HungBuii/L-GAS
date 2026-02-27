c6c4bccaa20a12674b46c8332272d682649ab75d
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2026-02-27 16:05:48 +0700

    create: AuraGameplayTag - Singleton containing native Gameplay Tags

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright Hung ne
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ *	"AuraGameplayTags"
+ *	
+ *	Singleton containing native Gameplay Tags
+ */
+
+struct FAuraGameplayTags
+{
+public:
+	static const FAuraGameplayTags& Get() { return GameplayTags; }
+	
+	static void InitializeNativeGameplayTags();
+protected:
+	
+private:
+	static FAuraGameplayTags GameplayTags;
+};
